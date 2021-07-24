a79ed7dac5fc66c9950475ee46125c443e80a137
victor <vajor@mgic.umaryland.edu>
2021-07-24 15:48:48 +0100

    added the home menu feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
+import Header from './components/header/Header';
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/"><Login/></Route>
+            <Route exact path="/home">
+              <Home/>
+              <Header/>
+            </Route>
           </Switch>
         </Router>
     </div>
---
+++ src/components/header/Header.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { Container, Content, Logo, Search, SearchIcon, Nav,
+NavListWrap, NavList, User, Work, SignOut } from './Header.styles'
+
+const Header = (props) => {
+    return (
+
+        <Container>
+            <Content>
+                <Logo>
+                    <a href="/home">
+                        <img src="/images/home-logo.svg" alt="home logo"/>
+                    </a>
+                </Logo>
+                <Search>
+                    <div>
+                        <input type="text" name="" placeholder="Search" />
+                    </div>
+                    <SearchIcon>
+                    <img src="/images/search-icon.svg" alt="" />
+                    </SearchIcon>
+                </Search>
+                <Nav>
+                   <NavListWrap>
+                       <NavList className="active">
+                            <a href="/home">
+                                <img src="/images/nav-home.svg" alt="nav home logo"/>
+                                <span>Home</span>
+                            </a>
+                       </NavList>
+                       <NavList>
+                            <a>
+                                <img src="/images/nav-network.svg" alt="nav home logo"/>
+                                <span>My network</span>
+                            </a>
+                       </NavList>
+                       <NavList>
+                            <a>
+                                <img src="/images/nav-jobs.svg" alt="nav home logo"/>
+                                <span>Jobs</span>
+                            </a>
+                       </NavList>
+                       <NavList>
+                            <a>
+                                <img src="/images/nav-messaging.svg" alt="nav home logo"/>
+                                <span>Messaging</span>
+                            </a>
+                       </NavList>
+                       <NavList>
+                            <a>
+                                <img src="/images/nav-notifications.svg" alt="nav home logo"/>
+                                <span>Notifications</span>
+                            </a>
+                       </NavList>
+                       <User>
+                            <a>
+                                <img src="/images/user.svg" alt="users" />
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg" alt="down" />
+                            </a>
+
+                            <SignOut>
+                            <a>
+                                Sign Out
+                            </a>
+                            </SignOut>
+                       </User>
+                       <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt="users" />
+                                <span>Work
+                                <img src="/images/down-icon.svg" alt="down" />
+                                </span>
+                            </a>
+                       </Work>
+                   </NavListWrap>
+                </Nav>
+            </Content>
+        </Container>
+    )
+}
+
+export default Header
